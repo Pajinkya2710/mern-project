cf9f0382c4be19eaf8a13f9c06c8aec1434e3319
Sandesh Kolhe <sandeshkolhe99@gmail.com>
2023-03-12 18:44:02 +0530

    adding for deploy

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ require("dotenv").config();
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 const profileRoutes = require("./routes/profileRoutes");
+const path = require('path')
+
 
 app.use(express.json());
 app.use(cors());
@@ -24,15 +26,21 @@ app.use("/api/tasks", taskRoutes);
 app.use("/api/profile", profileRoutes);
 
 
-// For deployment to heroku
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")));
-}
+// // For deployment to heroku
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+//   app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")));
+// }
 
+// static 
+app.use(express.static(path.join(__dirname, "./frontend/build")))
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname, "./frontend/build/index.html"))
+});
 
-
+//port
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Backend is running on port ${port}`);
 });
+
