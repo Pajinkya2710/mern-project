e017c7e2f9a9383cdbefd533f6e2d6bf23aa12b2
Sandesh Kolhe <sandeshkolhe99@gmail.com>
2023-03-12 23:42:18 +0530

    final

--- backend/app.js
+++ backend/app.js
@@ -26,20 +26,8 @@ app.use("/api/tasks", taskRoutes);
 app.use("/api/profile", profileRoutes);
 
 
-// // For deployment to heroku
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.resolve(__dirname, "../frontend/build")));
-//   app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")));
-// }
 
-// static 
-// app.use(express.static(path.join(__dirname, "./frontend/build")))
-// app.get('*', function(req, res){
-//   res.sendFile(path.join(__dirname, "./frontend/build/index.html"))
-// });
 
-//port
-// const port = process.env.PORT || 5000;
 const port=5000;
 app.listen(port, () => {
   console.log(`Backend is running on port ${port}`);
