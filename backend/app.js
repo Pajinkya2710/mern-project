1cf3a0b544b818da3b556cd39cb1a9872be15899
Sandesh Kolhe <sandeshkolhe99@gmail.com>
2023-03-12 20:33:39 +0530

    new one

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const path = require("path");
+// const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
 const authRoutes = require("./routes/authRoutes");
